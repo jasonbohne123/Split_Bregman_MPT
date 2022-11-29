3ea5d61048302a2831745c8b86dd52e40b42165e
jarrydscully <jarryds601@gmail.com>
2022-11-28 21:52:08 -0500

    Updated Covariance Estimation

--- Parameter_Estimation/bias_cov.py
+++ Parameter_Estimation/bias_cov.py
@@ -1,4 +1,25 @@
 import numpy as np
 
 def reg_cov(cov,penalty=0.5):
-    return (1-penalty)*cov+penalty*np.identity(cov.shape[0])
+    
+    cov = matrix.cov()
+
+    # Compute sample covariance matrix
+    sampleCov = np.dot(returns.values.T, returns.values) / len(matrix.columns)
+    sampleMean = matrix.mean()
+    
+    I = np.identity(len(cov.columns))
+    
+    # Compute the estimates
+    mu = np.dot(cov, I)
+    alpha = np.linalg.norm(cov - mu * I)
+    beta = np.linalg.norm(sampleCov - cov)
+    delta = np.linalg.norm(sampleCov - mu * I)
+
+    row1 = (beta**2)/(alpha**2 + beta**2)
+    row2 = (alpha**2)/(delta**2)
+    
+    covEstimate = row1 * mu * I + row2 * sampleCov  
+    
+    #(1-penalty)*cov+penalty*np.identity(cov.shape[0])
+    return covEstimate
