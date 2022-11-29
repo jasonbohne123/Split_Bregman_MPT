695277f561314b22c1b1fe3d85d6db9c3c750512
Jason Bohne <jbohne822@gmail.com>
2022-11-28 21:25:09 -0500

    reorg and structure

--- Split_Bregman/split_bregman_opt.py
+++ Split_Bregman/split_bregman_opt.py
@@ -1,105 +1,116 @@
 import numpy as np
-import scipy.optimize
+from scipy.optimize import Bounds, LinearConstraint, minimize
+from scipy.sparse import csc_matrix
 from qpsolvers import solve_qp
-from scipy.sparse import csc_matrix, csc_array
 
 
-
-def qp(cov,mean,w,d,b,beta,lambda2,tol,maxiter,approach,pen=True):
+def qp(cov, mean, w, d, b, lb, ub, lambda2, approach, tol=10e-6, maxiter=25, verbose=True):
     """ Inner optimization problem using unconstrained optimization 
-        Approach 1 solves numerically via unconstrained minimization
-        Approach 2 solves analytically via cvxopt within qpsolvers 
+        Approach "numerical" solves numerically via unconstrained minimization
+        Approach "closed-form" solves analytically via cvxopt within qpsolvers 
         Support for standard mean variance optimization (approach=2; pen=False)
     """
-    
-    x0=w # initial guess is previous result
-    
-    
-    if approach==1:
-        def objective_val(w,cov,mean,d,b,beta,lambda2):
-            obj=np.dot(np.dot(w,cov),w)-np.dot(w,mean)
 
-            if pen:
-                l2_pen=np.sum((d-beta*w-b)**2)
-            else:
-                l2_pen=0
+    # reinitialize unit starting point randomly to not get stuck in local minima
+    x0 = np.random.uniform(-1, 1, len(mean))
+    x0 = x0/np.linalg.norm(x0, ord=1)
 
-            return obj+lambda2*l2_pen/2
+    status = None
 
-        res=scipy.optimize.minimize(objective_val,x0=w,args=(returns_cov,returns_mean,d,b,beta,lambda2),method='CG',options={'maxiter':25},tol=1e-12)
-        w=res.x
-        
-    if approach==2:
-        
-        w_old=w
-        if pen:
-            quad=csc_matrix(2*(cov+2*lambda2*np.identity(len(mean))))
-            linear=-1*mean+lambda2*(b-d)
-        else:
-            quad=csc_matrix(2*(cov))
-            linear=-1*mean
-        
-        w = solve_qp(quad,linear,initvals=x0, solver="cvxopt")
+    if approach == "numerical":
+        def objective_val(w, cov, mean, d, b, lambda2):
+            obj = np.dot(np.dot(w, cov+lambda2*np.diag(d-b)), w) - \
+                np.dot(w, mean)
+
+            return obj
+        bounds = Bounds(lb, ub)
+        lc = LinearConstraint(np.ones(len(mean)), 1, 1)
+        res = minimize(objective_val, x0=x0, args=(cov, mean, d, b, lambda2),
+                       method='SLSQP', bounds=bounds, constraints=lc, options={'maxiter': maxiter}, tol=tol)
+        w = res.x
+        status = res.status
+
+    if approach == "closed-form":
+        w_old = w
+
+        # define objective function with l2 penalty
+        quad = csc_matrix((cov+lambda2*np.diag(d-b)))
+        linear = -1*mean
+
+        # constrain solution vector to be unit norm and non-negative
+        A = np.ones(len(mean))
+        constraint = np.array([1.0])
+
+        # set box constraints
+        lb_vec = lb*np.ones(len(mean))
+        ub_vec = ub*np.ones(len(mean))
+
+        w = solve_qp(quad, linear.reshape(len(linear),), A=A, b=constraint,
+                     initvals=x0, lb=lb_vec, ub=ub_vec, solver="cvxopt")
+        status = "solved"
 
         if w is None:
-            return w_old
-        
-    return w
+            print("QP Optimization fails, Trying again")
+            status = "failed"
+            return w_old, status
+    return w, status
 
-def shrinkage(w,d,b,lambda1,beta):
-    """ Applies soft-thresholding operator that is equivalent to L1 penalization 
+
+def shrinkage(w, b, lambda1, beta):
+    """ Applies one-sided soft-thresholding operator that is equivalent to L1 penalization 
     """
-    
-    x=beta*w+b
- 
-    obj=np.fmax([np.abs(x)-(1/lambda1)],[np.zeros(len(w))])  # elementwise max
-    signed_obj=np.sign(x)*obj
+
+    if lambda1 == 0:
+        return np.zeros(len(w))
+    x = beta*w+b
+    obj = np.fmax([np.abs(x)-(1/lambda1)], [np.zeros(len(w))])
+    signed_obj = np.sign(x)*obj
 
     return signed_obj[0]
 
-def split_bregman(cov,mean,lambda1,lambda2,beta=None,tol=1e-10,maxiter=100,approach=2):
+
+def split_bregman(cov, mean, lambda1, lambda2, lb, ub, beta=None, tol=1e-10, maxiter=100, approach=2, verbose=True):
     """ Split Bregman Optimization Routine
     """
 
-    error=1
-    n=len(mean)
+    total_cost = 1
+    n = len(mean)
     if beta is None:
-        beta=np.ones(n)
-    
-    b=np.zeros(n)
-    w=np.zeros(n)
-    d=np.zeros(n)
-    
-    
-    i=0
-    while error>tol:
-        if i>maxiter:
-            print("MaxIter Achieved")
+        beta = np.ones(n)
+
+    b = np.zeros(n)
+    w = np.zeros(n)
+    d = np.zeros(n)
+
+    i = 0
+    while total_cost > tol:
+        if i > maxiter:
+            if verbose:
+                print("MaxIter Achieved")
             break
-            
-        
-        if i%25==0 and i>0:
-            print(f"{i}-th iteration with error of {error}")
-    
-        w=qp(cov,mean,w,d,b,beta,lambda2,tol,maxiter,approach)
-        w=w/np.linalg.norm(w,ord=1)
-        
-        # sparse soln near w
-        d=shrinkage(w,d,b,lambda1,beta)
-        if sum(abs(d))!=0:
-            d=d/np.linalg.norm(d,ord=1)
-        
-        # b difference between l2 and l1
-        b=b+beta*w -d 
-        if sum(abs(b))!=0:
-            b=b/np.linalg.norm(b,ord=1)
-        i+=1
-        # error in terms of total soln
-        error=abs(np.dot(np.dot(w,cov),w)-np.dot(w.T,mean)+np.sum(abs(w))+np.sum(w**2))
-    
-    print(f"Terminated in {i} iterations of error {error} for lambda1={lambda1} and lambda2={lambda2}")
-    w=np.round(w,8)
-    if sum(abs(w))==0:
-        return w,error
-    unit_w=w/np.linalg.norm(w,ord=1)
-    return unit_w, error
+
+        # solve qp problem either closed form or numerically
+        w, status = qp(cov, mean, w, d, b, lb, ub, lambda2,
+                       approach, tol, maxiter, verbose)
+
+        if status == "failed":
+            w = w_old
+            break
+
+        # solve for sparse vector near previous optimal solution
+        d = shrinkage(w, b, lambda1, beta)
+        b = b+beta*w - d
+        i += 1
+
+        # compute total cost
+        total_cost = np.dot(np.dot(w, cov), w)-np.dot(w, mean) + \
+            (lambda1*np.sum(np.abs(w))+lambda2*np.sum((w)**2))/2
+        if verbose:
+            print(f"Total Cost: {total_cost}")
+
+        w_old = w
+
+    if sum(abs(w)) == 0:
+        return w, total_cost
+
+    return w, total_cost
